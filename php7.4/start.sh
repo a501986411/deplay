269e834d4a80b4113da0d5ae7e45c7acfbe2897f
chenhailong <hailong.chen@happy-seed.com>
2021-08-17 10:44:53 +0800

    首次

---
+++ php7.4/start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "start exec start.sh ..."
+cd /data/src/apollo_client_deploy
+chmod u+x apollo_client.bin
+nohup ./apollo_client.bin server -a poster-open-api,sales-wdmm-api -e dev -l 1 -c /data/config >> /dev/null 2>&1 &
+/usr/sbin/php-fpm
