91fdea6800744c52a316a57d6cc4b3e997b1cd45
chenhailong <hailong.chen@happy-seed.com>
2021-08-23 16:32:14 +0800

    新增swoole扩展

--- php7.4/start.sh
+++ php7.4/start.sh
@@ -2,5 +2,5 @@
 echo "start exec start.sh ..."
 cd /data/src/apollo_client_deploy
 chmod u+x apollo_client.bin
-nohup ./apollo_client.bin server -a poster-open-api,sales-wdmm-api -e dev -l 1 -c /data/config >> /dev/null 2>&1 &
+nohup ./apollo_client.bin server -a poster-open-api,sales-wdmm-api,wdmama-api -e dev -l 1 -c /data/config >> /dev/null 2>&1 &
 /usr/sbin/php-fpm
